954af7cfdc0e96aeaee1d44bac0c51e38ddb60dd
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-07-05 09:35:31 +0500

    fix

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 let h2 = document.querySelectorAll(".fruit");
 let sub = document.querySelectorAll(".sub1");
 let del = document.querySelectorAll(".del1");
@@ -10,6 +8,8 @@ let card1 = document.querySelector(".card1")
 let card2 = document.querySelector(".ca")
 let card3 = document.querySelector(".caa")
 let b = document.querySelector("b")
+let body = document.querySelector("body")
+
 sub.forEach(e => {
     e.addEventListener("click", (e) => {
         if (e.target.textContent === "Show price") {
@@ -19,13 +19,6 @@ sub.forEach(e => {
         }
     })
 });
-
-
-can.addEventListener("click", (e) => {
-    if (e.target.textContent === "Cancel") {
-        modal.classList.remove("modals-visible")
-    }
-})
 
 del.forEach(e => {
     e.addEventListener("click", (e) => {
@@ -37,27 +30,41 @@ del.forEach(e => {
             del1.addEventListener("click", () => {
                 card1.classList.add("cardfa")
                 modal.classList.remove("modals-visible")
+                body.style.background = "white"
+            body.style.transition = "1s"
             })
+            body.style.background = "gray"
+            body.style.transition = "1s"
         } else if (path === "two") {
             modal.classList.add("modals-visible")
             b.innerText = "Banan"
             del1.addEventListener("click", () => {
                 card2.classList.add("cardfa")
                 modal.classList.remove("modals-visible")
+                body.style.background = "white"
+            body.style.transition = "1s"
             })
+            body.style.background = "gray"
+            body.style.transition = "1s"
         } else if (path === "three") {
             modal.classList.add("modals-visible")
             b.innerText = "Anor"
             del1.addEventListener("click", () => {
                 card3.classList.add("cardfa")
                 modal.classList.remove("modals-visible")
+                body.style.background = "white"
+            body.style.transition = "1s"
             })
+            body.style.background = "gray"
+            body.style.transition = "1s"
         }
     })
 });
 
-
-
-
-
-
+can.addEventListener("click", (e) => {
+    if (e.target.textContent === "Cancel") {
+        modal.classList.remove("modals-visible")
+        body.style.background = "white"
+        body.style.transition = "1s"
+    }
+})
